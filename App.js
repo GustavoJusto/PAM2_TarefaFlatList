e07ffb57da669cd1cd29886deae450bb49e9e5d3
Gustavo justo <gustavojusto2006@gmail.com>
2021-09-02 21:04:03 -0300

    Ultimo commit, flatlist adicionada e funcionando, dados adicionados e style feito

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, FlatList} from 'react-native';
 import Curso from './Cursos';
+import { LinearGradient } from 'expo-linear-gradient';
 
 
 export default class App extends Component
@@ -12,11 +13,15 @@ export default class App extends Component
     {
       dados:
       [
-        {id:'1', nome:'Desenvolvimento de Sistemas', 
-        des:"Aprenda a desenvolver sistemas e projetos com várias linguagens de programação.", imagen:"DS.js"},
-        {/*{id:'2', nome:'Administração', des:"Programar top", imagen:"generica"},
-        {id:'3', nome:'Design digital', des:"Programar top", imagen:"generica"},
-      {id:'4', nome:'Mecanica', des:"Programar top", imagen:"generica"}*/}
+        {id:'1', nome:'Programação',des:"Aprenda a desenvolver sistemas e projetos com várias linguagens de programação.", imagen:"https://lh3.googleusercontent.com/proxy/-B1MDS1SYW8felJHSh2ZW_Up-h_3ybQHnBujG91BsDpzOQeD-LgZ7pDQiXkgOZqAqluIZOeqRUA"},
+        {id:'2', nome:'Administração', des:"Aprenda a gerir projetos e finanças assim como diversas outras áreas de contabilidade.", imagen:"https://uniateneu.edu.br/wp-content/uploads/2020/09/Administracao-min.jpg"},
+        {id:'3', nome:'Design digital', des:"Aprenda a desenvolver projetos de layout e organização de imagens com copetencia e proficionalidade.", imagen:"https://id7.com.br/wp-content/uploads/2014/11/design.jpg"},
+        {id:'4', nome:'Mecanica', des:"Aprenda tudo sobre mecanica de automoveis, indo desde reparo e manutenção de veiculos a projeto e contrução.", imagen:"https://cpfabbri.com.br/wp-content/uploads/2020/10/shutterstock_727170871-scaled.jpg"},
+        {id:'5', nome:'Arquitetura', des:"Aprenda a fazer a orgnização de interiores e a produção de abientes segundo diversas finalidades.", imagen:"https://www.weg.net/tomadas/blog/wp-content/uploads/2017/12/arquitetura-e-urbanismo.jpg"},
+        {id:'6', nome:'Jornalismo', des:"Você vai aprender a produzir materias para jornais, TV e mídias digitais, tambem aprendera como lidar com ameaças de morte frequentes.", imagen:"http://www.portalimprensa.com.br/content_file_storage/2019/10/07/jornalismo_certo.jpg"},
+        {id:'7', nome:'Letras', des:"Perdão ai Jussimar, mas já to sem ideia de descrição então vai essa mesmo.", imagen:"https://d3q93wnyp4lkf8.cloudfront.net/revista/post_images/4363/f1696b1137216942049f1aa51cb2f40f6dd72070.jpg?1506450790"},
+        {id:'8', nome:'Psicologia', des:"Cuida de louco seila.", imagen:"http://vestibular.unimes.br/wp-content/uploads/2017/06/Bacharel-em-psicologia.png"},
+
       ]
     }
   }
@@ -26,11 +31,13 @@ export default class App extends Component
   render()
   {
     return (
-      <View style={styles.container}>
+      <LinearGradient colors={['#2375d9','#96d7ff']}>
+      <View>
         <FlatList style={styles.FlatList}
         data={this.state.dados} keyExtractor={(item)=>item.id} renderItem={({item})=><Curso dados={item}/>}
         />
       </View>
+      </LinearGradient>
     );
   };
 }
@@ -38,7 +45,7 @@ export default class App extends Component
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#80a2d9',
+    backgroundColor: '#469cc2',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- Cursos.js
+++ Cursos.js
@@ -8,8 +8,10 @@ const Curso = (props) => {
     return (
       <View>
         <Text style={styles.Titulo}>{props.dados.nome}</Text>
-        <Image style={styles.Imagens} source={require(`./imagens/${props.dados.imagem}`)}/>
+        <Image style={styles.Imagens} source={{uri: props.dados.imagen}}/>
         <Text style={styles.corpo}> {props.dados.des}</Text>
+        <Text style={styles.DivisorPrimario}></Text>
+        <Text style={styles.Divisor2}></Text>
       </View>
     );
   };
@@ -20,12 +22,15 @@ const Curso = (props) => {
       {
         fontSize: 20,
         textAlign: 'center',
+        marginTop: 20,
       },
 
       Titulo:
       {
-        fontSize: 25,
+        fontSize: 30,
+        fontStyle: 'oblique',
         textAlign: 'center',
+        marginTop: 4,
       },
 
       Imagens:
@@ -34,7 +39,25 @@ const Curso = (props) => {
         height:250,
         marginLeft:32,
         marginTop:10,
-      }
+      },
+
+      DivisorPrimario:
+      {
+        borderBottomColor: '#f0f0f0',
+        borderBottomWidth: 3,
+        marginTop: 30,
+        marginStart: 20,
+        marginEnd: 20,
+      },
+
+      Divisor2:
+      {
+        borderBottomColor: '#ffaa00',
+        borderBottomWidth: 3,
+        marginTop: 10,
+        marginStart: 60,
+        marginEnd: 60,
+      },
   })
   
   export default Curso;
